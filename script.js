6fbd989733767286918600ff2672fec6d5af0eed
Germán <gmarera@gmail.com>
2025-03-24 00:59:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '0d4f8fa00641a4a0516979f0dce86ea6';
+const diffKelvin = 273.15;
+
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    const city = document.getElementById('cityInput').value;
+    if (city) {
+        fetchWeather(city);
+    } else {
+        alert('Por favor, ingresa una ciudad válida');
+    }
+});
+
+function fetchWeather(city) {
+    fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+        .then(data => data.json())
+        .then(data => showWeatherData(data))
+        };
+
+
+function showWeatherData(data) {
+    const divResponseData = document.getElementById('responseData');
+    divResponseData.innerHTML = '';
+
+    const cityName = data.name;
+    const countryName = data.sys.country;
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    const cityInfo = document.createElement('h2');
+    cityInfo.textContent = `${cityName}, ${countryName}`;
+    
+    const tempInfo = document.createElement('p');
+    tempInfo.textContent = `Temperatura: ${Math.floor(temp - diffKelvin)}°C`;
+
+    const humidityInfo = document.createElement('p');
+    humidityInfo.textContent = `Humedad: ${humidity}%`;
+
+    const iconInfo = document.createElement('img');
+    iconInfo.src = `http://openweathermap.org/img/w/${icon}.png`;
+
+    const descriptionInfo = document.createElement('p');
+    descriptionInfo.textContent = description;
+
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(tempInfo);
+    divResponseData.appendChild(humidityInfo);
+    divResponseData.appendChild(iconInfo);
+    divResponseData.appendChild(descriptionInfo);
+}
